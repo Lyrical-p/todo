a2eb80d5da640e5e967f6701ea2dbb9c8e9ecf8f
Lyyrical-p <prymrlyrical@gmail.com>
2025-10-07 17:09:24 +0000

    Add css styling

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,8 +1,27 @@
 /** @format */
 
+import { useState } from 'react'
+import { RiDeleteBin6Fill } from 'react-icons/ri'
+import { TiInputChecked } from 'react-icons/ti'
 import './App.css'
 
 function App() {
+  const [isCompleted, setIsCompleted] = useState(false)
+  const [allTodos, setTodos] = useState([])
+  const [newTitle, setNewTitle] = useState('')
+  const [newDesc, setNewDesc] = useState('')
+
+  const handleAddTodo = () => {
+    let newTodo = {
+      title: newTitle,
+      description: newDesc,
+    }
+
+    let updatedTodo = [...allTodos]
+    updatedTodo.push(newTodo)
+    setTodos(updatedTodo)
+  }
+
   return (
     <div className='App'>
       <h1>My Todo List</h1>
@@ -10,27 +29,64 @@ function App() {
         <div className='input'>
           <div className='input-item'>
             <label>Title</label>
-            <input type='text' placeholder='Enter title' />
+            <input
+              type='text'
+              value={newTitle}
+              onChange={(e) => setNewTitle(e.target.value)}
+              placeholder='Enter title'
+            />
           </div>
           <div className='input-item'>
             <label>Description</label>
-            <input type='text' placeholder='Enter description' />
+            <input
+              type='text'
+              value={newDesc}
+              onChange={(e) => setNewDesc(e.target.value)}
+              placeholder='Enter description'
+            />
           </div>
           <div className='input-item'>
-            <button type='button' className='primaryBtn'>
+            <button
+              type='button'
+              onClick={handleAddTodo}
+              className='primaryBtn'
+            >
               Add Todo
             </button>
           </div>
         </div>
         <div className='btn'>
-          <button>TODO</button>
-          <button>Completed</button>
+          <button
+            className={`secondaryBtn ${isCompleted === false && 'active'}`}
+            onClick={() => setIsCompleted(false)}
+          >
+            TODO
+          </button>
+          <button
+            className={`secondaryBtn ${isCompleted === true && 'active'}`}
+            onClick={() => setIsCompleted(true)}
+          >
+            Completed
+          </button>
         </div>
         <div className='todo-list'>
-          <div className='todo-item'>
-            <h3>Title</h3>
-            <p>Description</p>
-          </div>
+          {allTodos.map((item, index) => {
+            return (
+              <div className='todo-item' key={index}>
+                <div>
+                  <h3>{item.title}</h3>
+                  <p>{item.description}</p>
+                </div>
+                <div>
+                  <RiDeleteBin6Fill className='icon' />
+                  <TiInputChecked
+                    className='check-icon'
+                    title='Do you want to complete it?'
+                  />
+                </div>
+              </div>
+            )
+          })}
         </div>
       </div>
     </div>
